97031c021217e28547accfa53371c86a1e6963d8
Philbert Lin <philbert.lin@intel.com>
2014-09-03 14:07:11 -0700

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# Philbert Lin
+# Set up script for Ubuntu Desktop
 #
-# export http_proxy=
-# export https_proxy=
-# sudo apt-get install -y git
+# Do these first in shell
+# $ export http_proxy=
+# $ export https_proxy=
+# $ sudo apt-get install -y git
 #
 # Get dotfiles
-# git clone https://github.com/philin/dotfiles.git
-#
-# Then run ./setup.sh
+# $ git clone https://github.com/philin/dotfiles.git
+# $ chmod ~/dotfiles/setup.sh
+# $ .dotfiles/setup.sh
 
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.screenrc ~/.screenrc
@@ -17,4 +20,7 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install -y exuberant-ctags
 wget http://cscope.sourceforge.net/cscope_maps.vim ~/.vim/.
 vim +PluginInstall +qall
+
+# Utility tools
+sudo apt-get install -y byobu tree
 
